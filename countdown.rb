1fa9c9cc6ae2d537843be146a67e3f13c48b510b
Sav Younts <savyounts@gmail.com>
2018-08-03 16:50:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,11 @@ end
 puts "HAPPY NEW YEAR!"
 
 def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end
   
+end
+puts "HAPPY NEW YEAR!
