1befcd98a9956d6fa66b0356c502cc786852a5fa
Sav Younts <savyounts@gmail.com>
2018-08-03 16:52:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,9 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1 
   end
-  
+  puts "HAPPY NEW YEAR!"
 end
-puts "HAPPY NEW YEAR!"
+
 
 def countdown_with_sleep(number)
   while number > 0 
